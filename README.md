afd062dabfba061219bc6c8464eafd8902b961d6
ferretcode <674313@salemkeizer.org>
2022-12-25 22:35:14 -0800

    add CLI generator

---
+++ README.md
@@ -0,0 +1,11 @@
+# goated-generator
+
+A CLI tool for cloning templates of goated.js projects
+
+## Usage
+
+- Create your template folder/file inside of ~/goated.js/templates
+- `npx goated-generator`
+- Choose your template
+- Name it
+- And you're done!
---
+++ index.js
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+
+const figlet = require("figlet");
+const inquirer = require("inquirer");
+const fs = require("fs-extra");
+const os = require("os");
+const multispinner = require("multispinner");
+
+figlet("goated", async (err, data) => {
+  if (err) return console.error("There was an error starting the CLI!");
+
+  console.log(data);
+
+  const templatesPath = `${os.homedir()}/goated.js/templates`;
+
+  if (!fs.existsSync(templatesPath)) {
+    try {
+      fs.mkdirSync(`${os.homedir()}/goated.js`);
+			fs.mkdir(templatesPath);
+
+      return console.log("You have no templates to clone!");
+    } catch (err) {
+      return console.error(
+        `There was an error creating the templates directory: ${err}`
+      );
+    }
+  }
+
+  const templates = fs.readdirSync(templatesPath);
+
+	console.log(templates);
+
+  if (!templates.length) return console.log("You have no templates to clone!");
+
+  const answers = await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "template",
+        message: "Which template do you want to clone?",
+        default: templates[0],
+        choices: templates,
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the new app?",
+        default: templates[0],
+      },
+    ])
+    .catch((err) =>
+      console.error(`There was an error cloning the template: ${err}`)
+    );
+
+  const spinner = new multispinner([answers.template], {
+    preText: "Cloning template",
+  });
+
+  try {
+    fs.copySync(`${templatesPath}/${answers.template}`, `./${answers.name}`);
+
+    spinner.success(answers.template);
+  } catch (err) {
+    console.error(`There was an error cloning the template: ${err}`);
+  }
+});
